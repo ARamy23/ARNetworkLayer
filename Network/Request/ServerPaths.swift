353d234b90f8647e364a1c3f65d777aa086b5eab
Ahmed Ramy <Dev.AhmedRamy@iCloud.com>
2018-07-08 10:30:49 +0200

    Initial Commit

---
+++ Network/Request/ServerPaths.swift
@@ -0,0 +1,14 @@
+//
+//  ServerPaths.swift
+//  WiFi Metropolis
+//
+//  Created by Ahmed Ramy on 6/24/18.
+//  Copyright © 2018 Ahmed Ramy. All rights reserved.
+//
+
+import Foundation
+
+struct ServerPath
+{
+    // write static properties here
+}
